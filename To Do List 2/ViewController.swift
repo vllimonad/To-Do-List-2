8cd2952e3b9e290e88d40f9753176ea9ebbd0134
Vlad Klunduk <klundukvlad40@gmail.com>
2023-10-13 21:04:50 +0200

    Initial Commit

---
+++ To Do List 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To Do List 2
+//
+//  Created by Vlad Klunduk on 13/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
